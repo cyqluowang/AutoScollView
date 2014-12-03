324348053ab254b222561c70f6a01ca89fc9a1c3
cyq <cyqluowang@gmail.com>
2014-12-03 20:35:49 +0800

    first

---
+++ src/com/cyq/autoscollview/ImagePagerAdapter.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2014 trinea.cn All right reserved. This software is the confidential and proprietary information of
+ * trinea.cn ("Confidential Information"). You shall not disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into with trinea.cn.
+ */
+package com.cyq.autoscollview;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+/**
+ * ImagePagerAdapter
+ * 
+ * @author <a href="http://www.trinea.cn" target="_blank">Trinea</a> 2014-2-23
+ */
+public class ImagePagerAdapter extends RecyclingPagerAdapter {
+
+    private Context       context;
+    private List<Integer> imageIdList;
+
+    private int           size;
+    private boolean       isInfiniteLoop;
+
+    public ImagePagerAdapter(Context context, List<Integer> imageIdList) {
+        this.context = context;
+        this.imageIdList = imageIdList;
+        this.size = ListUtils.getSize(imageIdList);
+        isInfiniteLoop = false;
+    }
+
+    @Override
+    public int getCount() {
+        // Infinite loop
+        return isInfiniteLoop ? Integer.MAX_VALUE : ListUtils.getSize(imageIdList);
+    }
+
+    /**
+     * get really position
+     * 
+     * @param position
+     * @return
+     */
+    private int getPosition(int position) {
+        return isInfiniteLoop ? position % size : position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup container) {
+        ViewHolder holder;
+        if (view == null) {
+            holder = new ViewHolder();
+            view = holder.imageView = new ImageView(context);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder)view.getTag();
+        }
+        holder.imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        holder.imageView.setImageResource(imageIdList.get(getPosition(position)));
+        view.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(context, "默认Toast样式",
+					     Toast.LENGTH_SHORT).show();Toast.makeText(context, "默认Toast样式",
+					    	     Toast.LENGTH_SHORT).show();
+				
+			}
+		});
+        return view;
+    }
+
+    private static class ViewHolder {
+
+        ImageView imageView;
+    }
+
+    /**
+     * @return the isInfiniteLoop
+     */
+    public boolean isInfiniteLoop() {
+        return isInfiniteLoop;
+    }
+
+    /**
+     * @param isInfiniteLoop the isInfiniteLoop to set
+     */
+    public ImagePagerAdapter setInfiniteLoop(boolean isInfiniteLoop) {
+        this.isInfiniteLoop = isInfiniteLoop;
+        return this;
+    }
+}
